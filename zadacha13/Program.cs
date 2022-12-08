0ed0b3e0cf9d299abe24c113235a4bb0b8cec17e
Fedor <fedor.kochkin48@gmail.com>
2022-12-08 23:24:09 +0300

    решение задачи № 13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int chislo = 0;
+Console.WriteLine("введите a");
+int a = int.Parse(Console.ReadLine());
+while (a >= 1000)
+{
+    a = a / 10;
+}
+chislo = a % 10;
+  Console.WriteLine(chislo); 
