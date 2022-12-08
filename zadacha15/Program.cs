214b74c5b3bec5682ad93e49d0af0169b387c93c
Fedor <fedor.kochkin48@gmail.com>
2022-12-09 02:30:50 +0300

    решение задачи № 15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("введите цифру");
+int a = int.Parse(Console.ReadLine());
+if (a < 1 | a > 7)
+Console.WriteLine("данное число не является номером дня недели");
+if (a >= 1 & a <= 5)
+Console.WriteLine("будний день");
+if (a >= 6 & a <= 7)
+Console.WriteLine("выходной день");
